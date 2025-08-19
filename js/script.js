126bff057ea43ee95ecbbc19586491c1d1efe381
Mykhailo Khashchevatskyi <democracy.potato@gmail.com>
2025-08-19 20:44:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const burgerMenu = document.getElementById('burger-menu');
+const menu = document.querySelector('.header__menu');
+const menuItems = document.querySelectorAll('.header__item');
+
+burgerMenu.addEventListener('click', () => {
+	burgerMenu.classList.toggle('open');
+	menu.classList.toggle('open');
+
+	// Забороняємо скрол на body при відкритті меню
+	if (menu.classList.contains('open')) {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = '';
+	}
+});
+
+// Додаємо обробник події для кожного елемента меню
+menuItems.forEach(item => {
+	item.addEventListener('click', () => {
+		if (menu.classList.contains('open')) {
+			burgerMenu.classList.remove('open');
+			menu.classList.remove('open');
+
+			// Відновлюємо скрол після закриття меню
+			document.body.style.overflow = '';
+		}
+	});
+});
